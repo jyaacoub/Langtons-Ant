67a78e27656b36169028a3a7a0b3bc4810edc472
jyaacoub <50300488+jyaacoub@users.noreply.github.com>
2020-06-20 15:34:59 -0400

    ants and grid

---
+++ LangtonsAnt/Assets/GridManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField]
+    private int rows = 5;
+    [SerializeField]
+    private int cols = 8;
+    [SerializeField]
+    public static float tileSize = 1.02f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    private void GenerateGrid()
+    {
+        GameObject referanceTile = (GameObject)Instantiate(Resources.Load("tile_white"));
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                GameObject tile = (GameObject)Instantiate(referanceTile, transform);
+
+                float posX = col * tileSize;
+                float posY = row * -tileSize;
+
+                tile.transform.position = new Vector2(posX, posY);
+            }
+        }
+
+        Destroy(referanceTile);
+
+        // Centering the grid:
+        float gridWidth = cols * tileSize;
+        float gridHeight = rows * tileSize;
+        transform.position = new Vector2((tileSize - gridWidth)/2, (gridHeight - tileSize)/2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ LangtonsAnt/Assets/LangtonsAnt.cs
@@ -0,0 +1,51 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class LangtonsAnt : MonoBehaviour
+{
+    public float speed = 128;
+    public float tileSize = GridManager.tileSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Bringing it to the center of the right tile.
+        transform.position += new Vector3(tileSize / 2, 0, 0);
+    }
+
+    private void moveAnt()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(tileSize, 0, 0);
+            transform.eulerAngles = new Vector3(0, 0, -90);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position -= new Vector3(tileSize, 0, 0);
+            transform.eulerAngles = new Vector3(0, 0, 90);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, tileSize, 0);
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.position -= new Vector3(0, tileSize, 0);
+            transform.eulerAngles = new Vector3(0, 0, 180);
+        }
+    }
+}
